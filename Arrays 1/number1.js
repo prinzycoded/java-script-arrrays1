6ca96fac94ce56718ad8c6d95c35aed718d6cd7c
Prince Ugochukwu <princeugo814@gmail.com>
2026-03-27 12:57:39 +0100

    my number three

--- Arrays 1/number1.js
+++ Arrays 1/number1.js
@@ -1,11 +1,13 @@
-// Create the book object
+function printbookdetails() {
+
 let book = {
   title: "The Great Adventure",
   pages: 250,
   isAvailable: true
 };
-
-// Print each property to the console
 console.log(book.title);
 console.log(book.pages);
 console.log(book.isAvailable);
+}
+printbookdetails();
+
