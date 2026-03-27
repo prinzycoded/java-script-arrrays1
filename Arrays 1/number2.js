c8a9ae87fe5385f675bfc81a27e48be8682365b2
Prince Ugochukwu <princeugo814@gmail.com>
2026-03-27 12:48:26 +0100

    my number two

---
+++ Arrays 1/number2.js
@@ -0,0 +1,17 @@
+let car = {
+  brand: "Toyota",
+  model: "Corolla",
+  year: 2019,
+  price: 5500000
+};
+console.log(car.brand);
+console.log(car.model);
+
+car.price = 4800000;
+console.log(car.price);
+
+car.color = "black";
+console.log(car.color);
+
+car.brand = "BENZ";
+console.log(car.brand);
